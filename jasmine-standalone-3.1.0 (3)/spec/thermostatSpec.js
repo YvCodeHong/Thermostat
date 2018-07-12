eb5a010f954a38f363e3e23a9ee08398fcd58c72
Yvonne Tang <ytang604@gmail.com>
2018-07-12 20:44:51 +0100

    add instances of thermostat

--- jasmine-standalone-3.1.0 (3)/spec/thermostatSpec.js
+++ jasmine-standalone-3.1.0 (3)/spec/thermostatSpec.js
@@ -1,5 +1,56 @@
 'use strict';
 
-describe('thermostat',function() {
+describe('Thermostat', function() {
 
+   var thermostat;
+
+   beforeEach(function() {
+     thermostat = new Thermostat();
+   });
+
+   it('starts at 20 degrees', function() {
+     expect(thermostat.temperature).toEqual(20);
+   });
+
+   it('increases in temperature with up ()', function() {
+     thermostat.up();
+     expect(thermostat.getCurrentTemperature()).toEqual(21);
+   })
+
+   it('decreases in temperature with down()', function(){
+     thermostat.down();
+     expect(thermostat.getCurrentTemperature()).toEqual(19)
+   })
+
+   it('has minimum of 10 degrees', function() {
+     for (var i = 0; i < 11; i++) {
+       thermostat.down();
+     }
+     expect(thermostat.getCurrentTemperature()).toEqual(10);
+   });
+
+   it('has power saving mode on by default', function() {
+     thermostat.powerSavingMode
+     expect(thermostat.isPowerSavingModeOn()).toBe(true);
+   });
+
+   it('can switch PSM off', function() {
+     thermostat.switchPowerSavingModeOff();
+     expect(thermostat.isPowerSavingModeOn()).toBe(false);
+   });
+
+   it('can switch PSM back on', function() {
+     thermostat.switchPowerSavingModeOff();
+     expect(thermostat.isPowerSavingModeOn()).toBe(false);
+     thermostat.switchPowerSavingModeOn();
+     expect(thermostat.isPowerSavingModeOn()).toBe(true);
+   });
 });
+
+
+
+
+
+
+
+//
